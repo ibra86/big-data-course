43e795b5b0fceab998ed32424d577ab423227ed2
Anton Ibragimov <ibrahimov.ag@gmail.com>
2021-12-21 20:45:51 +0200

    hw1 app

---
+++ hw_01/constants.py
@@ -0,0 +1,3 @@
+APP_NAME = 'robot-dreams-de-api'
+CONFIG_FILE_NAME = 'config.yaml'
+DATA_PATH = 'data'
---
+++ hw_01/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+import colorlog
+
+from constants import APP_NAME
+
+colorlog.basicConfig(level=logging.INFO)
+logger = colorlog.getLogger(APP_NAME)
